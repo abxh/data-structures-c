d01f948156c833be56a4f3eae95c90ad6d95777f
Shamim <abir09bs@gmail.com>
2024-01-21 17:18:37 +0100

    fix bug

--- implementations/realloc_array.c
+++ implementations/realloc_array.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 /* reallocate array such that it dynamically grows and shrinks as needed. */
-bool realloc_array(void* arr_p, size_t* capacity_p, size_t size) {
+bool realloc_array(void** arr_pp, size_t* capacity_p, size_t size) {
     size_t capacity_new = 0;
     if (size == *capacity_p) {
         capacity_new = *capacity_p << 1;
@@ -11,10 +11,11 @@ bool realloc_array(void* arr_p, size_t* capacity_p, size_t size) {
     } else {
         return true;
     }
-    void* data_p_new = realloc(arr_p, capacity_new);
-    if (data_p_new == NULL) {
+    void* arr_p_new = realloc(*arr_pp, capacity_new);
+    if (arr_p_new == NULL) {
         return false;
     }
     *capacity_p = capacity_new;
+    *arr_pp = arr_p_new;
     return true;
 }
