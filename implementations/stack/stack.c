184eec96072466b949a09e137b30d68a6b28c665
Shamim <abir09bs@gmail.com>
2024-01-27 12:06:23 +0100

    rewrite stack as dym arr, and reduce macro LOC

--- implementations/stack/stack.c
+++ implementations/stack/stack.c
@@ -1,47 +1,71 @@
+#include <stdio.h>
+#include <errno.h>
+#include <assert.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
 #include "stack.h"
 
-Stack* stack_new(void) {
-    return (Stack*)calloc(1, sizeof(Stack));
+Stack* stack_new(size_t data_size) {
+    Stack* stack_p = malloc(sizeof(Stack));
+    if (stack_p == NULL) {
+        return NULL;
+    }
+    stack_p->data_size = data_size;
+    stack_p->used_bytes = 0;
+    stack_p->capacity_bytes = data_size;
+    stack_p->darr = malloc(data_size);
+    if (stack_p->darr == NULL) {
+        free(stack_p);
+        return NULL;
+    }
+    return stack_p;
 }
 
 bool stack_isempty(const Stack* stack_p) {
-    return *stack_p == NULL;
+    return stack_p->used_bytes == 0;
 }
 
 void* stack_peek(const Stack* stack_p) {
-    return (*stack_p)->value_p;
+    assert(stack_p->used_bytes != 0);
+    return stack_p->darr;
 }
 
-bool stack_push(Stack* stack_p, void* value_p) {
-    StackNode* head_p = malloc(sizeof(StackNode));
-    if (head_p == NULL) {
-        return false;
-    }
-    head_p->value_p = value_p;
-    head_p->next_p = *stack_p;
-    *stack_p = head_p;
+bool stack_push(Stack* stack_p, void* value_p, size_t size) {
+    assert(size == stack_p->data_size);
+    if (stack_p->used_bytes == stack_p->capacity_bytes) {
+        void* new_darr = reallocarray(stack_p->darr, stack_p->capacity_bytes << 1, stack_p->data_size);
+        if (new_darr == NULL) {
+            errno = ENOMEM;
+            perror("realloc stack array");
+            return false;
+        }
+        stack_p->capacity_bytes <<= 1;
+        stack_p->darr = new_darr;
+    } 
+    memcpy(stack_p->darr + stack_p->used_bytes, value_p, stack_p->data_size);
+    stack_p->used_bytes += stack_p->data_size;
     return true;
 }
 
-void* stack_pop(Stack* stack_p) {
-    void* value_p = (*stack_p)->value_p;
-    StackNode* head_p_new = (*stack_p)->next_p;
-    free(*stack_p);
-    *stack_p = head_p_new;
-    return value_p;
+void* stack_pop(Stack* stack_p, size_t size) {
+    assert(stack_p->used_bytes != 0);
+    assert(size == stack_p->data_size);
+    if ((stack_p->used_bytes >> 2) == stack_p->capacity_bytes) {
+        void* new_darr = realloc(stack_p->darr, (stack_p->capacity_bytes >> 1) * stack_p->data_size);
+        if (new_darr == NULL) {
+            errno = ENOMEM;
+            perror("realloc stack array");
+            return NULL;
+        }
+        stack_p->capacity_bytes >>= 1;
+        stack_p->darr = new_darr;
+    } 
+    stack_p->used_bytes -= stack_p->data_size;
+    return stack_p->darr + stack_p->used_bytes;
 }
 
 void stack_free(Stack* stack_p) {
-    StackNode* head_p = *stack_p;
-    StackNode* next_p = NULL;
-    while (head_p != NULL) {
-        next_p = head_p->next_p;
-        free(head_p->value_p);
-        free(head_p);
-        head_p = next_p;
-    }
+    free(stack_p->darr);
     free(stack_p);
 }
+
--- implementations/stack/stack.h
+++ implementations/stack/stack.h
@@ -1,78 +1,46 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct StackNode {
-    void* value_p;
-    struct StackNode* next_p;
-} StackNode;
+typedef struct {
+    size_t data_size;
+    size_t used_bytes;
+    size_t capacity_bytes;
+    uint8_t* darr;
+} Stack;
 
-typedef StackNode* Stack;
+/* Try create a new stack and returns it's pointer. Returns NULL if OOM. */
+Stack* stack_new(size_t data_size);
 
-/* Create a new stack and return it's pointer. can return NULL and it should
- * be checked manually.*/
-Stack* stack_new(void);
-
-/* Return if the stack is empty. */
+/* Check if stack is empty */
 bool stack_isempty(const Stack* stack_p);
 
-/* Peek at the next value pointer to be popped. Must check if stack is empty
- * beforehand.*/
+/* Peek at the next value to be popped. Should check that stack is not empty beforehand. */
 void* stack_peek(const Stack* stack_p);
 
-/* Push a heap allocated value pointer onto the stack. Returns if successful.*/
-bool stack_push(Stack* stack_p, void* value_p);
+/* Push a value onto the stack. */
+bool stack_push(Stack* stack_p, void* value_p, size_t size);
 
-/* Pop the stack and return the heap allocated value pointer. Must check if stack
- * is empty beforehand.*/
-void* stack_pop(Stack* stack_p);
+/* Pop a value from the stack. Should check that stack is not empty beforehand. */
+void* stack_pop(Stack* stack_p, size_t size);
 
-/* Free the memory of stack appropiately.*/
+/* Free the memory of the stack appropiately. */
 void stack_free(Stack* stack_p);
 
-/* Create inline functions to directly work with stack values with appropiate
- * memory handling.*/
-#define CREATE_STACK_INLINE_FUNCTIONS(name, type, default_)                       \
-    static inline type stack_peek_##name(const Stack* stack_p) {                  \
-        void* value_p = stack_peek(stack_p);                                      \
-        if (value_p == NULL) {                                                    \
-            fprintf(stderr, "Error: Stack is empty. Returning default value.");   \
-            return (default_);                                                    \
-        }                                                                         \
-        return *(type*)value_p;                                                   \
-    }                                                                             \
-    static inline bool stack_push_##name(Stack* stack_p, type value) {            \
-        void* value_p = malloc(sizeof(type));                                     \
-        if (value_p == NULL) {                                                    \
-            return false;                                                         \
-        }                                                                         \
-        memcpy(value_p, &value, sizeof(type));                                    \
-        if (stack_push(stack_p, value_p)) {                                       \
-            return true;                                                          \
-        }                                                                         \
-        free(value_p);                                                            \
-        return false;                                                             \
-    }                                                                             \
-    static inline type stack_pop_##name(Stack* stack_p) {                         \
-        void* value_p = stack_pop(stack_p);                                       \
-        if (value_p == NULL) {                                                    \
-            fprintf(stderr, "Error: Stack is empty. Returning default value.");   \
-            return (default_);                                                    \
-        }                                                                         \
-        type value = *(type*)value_p;                                             \
-        free(value_p);                                                            \
-        return value;                                                             \
-    }                                                                             \
-    static inline type stacknode_get_##name(StackNode* stacknode_p) {             \
-        void* value_p = stacknode_p->value_p;                                     \
-        if (value_p == NULL) {                                                    \
-            fprintf(stderr, "Error: Node is NULL. Returning default value.");     \
-            return (default_);                                                    \
-        }                                                                         \
-        return *(type*)value_p;                                                   \
-    }                                                                             \
-    static inline void stacknode_set_##name(StackNode* stacknode_p, type value) { \
-        memcpy(stacknode_p->value_p, &value, sizeof(type));                       \
+/* Create inline functions to directly work with stack values. */
+#define CREATE_STACK_INLINE_FUNCTIONS(name, type, default_value)                                     \
+    static inline Stack* stack_new_##name(void) {                                                    \
+        return stack_new(sizeof(type));                                                              \
+    }                                                                                                \
+    static inline type stack_peek_##name(Stack* stack_p) {                                           \
+        return !stack_isempty(stack_p) ? (*(type*)stack_peek(stack_p)) : (default_value);            \
+    }                                                                                                \
+    static inline bool stack_push_##name(Stack* stack_p, type value) {                               \
+        return stack_push(stack_p, &value, sizeof(type));                                            \
+    }                                                                                                \
+    static inline type stack_pop_##name(Stack* stack_p) {                                            \
+        return !stack_isempty(stack_p) ? *(type*)stack_pop(stack_p, sizeof(type)) : (default_value); \
     }
