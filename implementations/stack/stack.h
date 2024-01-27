c6bb52f9ce8f08984faf14b75df43f3b8d147967
Shamim <abir09bs@gmail.com>
2024-01-27 12:30:49 +0100

    remove default value

--- implementations/stack/stack.h
+++ implementations/stack/stack.h
@@ -21,7 +21,7 @@ bool stack_isempty(const Stack* stack_p);
 /* Peek at the next value to be popped. Should check that stack is not empty beforehand. */
 void* stack_peek(const Stack* stack_p);
 
-/* Push a value onto the stack. */
+/* Try push a value onto the stack. Returns false if OOM. */
 bool stack_push(Stack* stack_p, void* value_p, size_t size);
 
 /* Pop a value from the stack. Should check that stack is not empty beforehand. */
@@ -31,16 +31,16 @@ void* stack_pop(Stack* stack_p, size_t size);
 void stack_free(Stack* stack_p);
 
 /* Create inline functions to directly work with stack values. */
-#define CREATE_STACK_INLINE_FUNCTIONS(name, type, default_value)                                     \
-    static inline Stack* stack_new_##name(void) {                                                    \
-        return stack_new(sizeof(type));                                                              \
-    }                                                                                                \
-    static inline type stack_peek_##name(Stack* stack_p) {                                           \
-        return !stack_isempty(stack_p) ? (*(type*)stack_peek(stack_p)) : (default_value);            \
-    }                                                                                                \
-    static inline bool stack_push_##name(Stack* stack_p, type value) {                               \
-        return stack_push(stack_p, &value, sizeof(type));                                            \
-    }                                                                                                \
-    static inline type stack_pop_##name(Stack* stack_p) {                                            \
-        return !stack_isempty(stack_p) ? *(type*)stack_pop(stack_p, sizeof(type)) : (default_value); \
+#define CREATE_STACK_INLINE_FUNCTIONS(name, type)                      \
+    static inline Stack* stack_new_##name(void) {                      \
+        return stack_new(sizeof(type));                                \
+    }                                                                  \
+    static inline type stack_peek_##name(Stack* stack_p) {             \
+        return (*(type*)stack_peek(stack_p));                          \
+    }                                                                  \
+    static inline bool stack_push_##name(Stack* stack_p, type value) { \
+        return stack_push(stack_p, &value, sizeof(type));              \
+    }                                                                  \
+    static inline type stack_pop_##name(Stack* stack_p) {              \
+        return *(type*)stack_pop(stack_p, sizeof(type));               \
     }
