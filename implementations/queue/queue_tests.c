dca1f9a670d9d0a002a3923e771d5de27b368b05
Shamim <abir09bs@gmail.com>
2024-02-08 00:13:56 +0100

    update test

--- implementations/queue/queue_tests.c
+++ implementations/queue/queue_tests.c
@@ -9,7 +9,7 @@
 
 bool empty_test(void) {
     Queue* queue_p = queue_new(1, sizeof(int));
-    bool res = true; 
+    bool res = true;
     queue_isempty(queue_p);
     queue_free(queue_p);
     return res;
@@ -32,12 +32,31 @@ bool one_element_test(void) {
 }
 
 bool million_elements_test(void) {
-    Queue* queue_p = queue_new(1000000,sizeof(int));
+    Queue* queue_p = queue_new(1000000, sizeof(int));
     bool res = true;
-    for (int i = 1; i <= 2; i++) {
+    for (int i = 1; i <= 1000000; i++) {
         queue_enqueue(queue_p, (unsigned char*)&i);
     }
-    for (int i = 1; i <= 2; i++) {
+    for (int i = 1; i <= 1000000; i++) {
+        res &= i == *(int*)queue_dequeue(queue_p);
+    }
+    queue_free(queue_p);
+    return res;
+}
+
+bool zigzag_test(void) {
+    Queue* queue_p = queue_new(1000000, sizeof(int));
+    bool res = true;
+    for (int i = 1; i <= 750000; i++) {
+        queue_enqueue(queue_p, (unsigned char*)&i);
+    }
+    for (int i = 1; i <= 750000; i++) {
+        res &= i == *(int*)queue_dequeue(queue_p);
+    }
+    for (int i = 1; i <= 750000; i++) {
+        queue_enqueue(queue_p, (unsigned char*)&i);
+    }
+    for (int i = 1; i <= 750000; i++) {
         res &= i == *(int*)queue_dequeue(queue_p);
     }
     queue_free(queue_p);
@@ -52,8 +71,10 @@ typedef struct {
 } func_plus;
 
 int main(void) {
-    func_plus bool_f_arr[] = {
-        {empty_test, "empty test"}, {one_element_test, "one element test"}, {million_elements_test, "million elements test"}};
+    func_plus bool_f_arr[] = {{empty_test, "empty test"},
+                              {one_element_test, "one element test"},
+                              {million_elements_test, "million elements test"},
+                              {zigzag_test, "zigzag test"}};
     for (size_t i = 0; i < sizeof(bool_f_arr) / sizeof(func_plus); i++) {
         printf("[%s] %s\n", bool_f_arr[i].func() ? "true" : "false", bool_f_arr[i].desc);
     }
