dc03bfcc1c878330eb80975cb5d09f86314443ec
Shamim <abir09bs@gmail.com>
2024-02-09 21:50:23 +0100

    simplify code

--- problems/matching_pair_check/matching_pair_check.c
+++ problems/matching_pair_check/matching_pair_check.c
@@ -3,43 +3,58 @@
 #include "stack.h"
 
 typedef enum {
-    UNDEFINED_SIGN_ENUM = 0,
+    SYMBOL_ENUM_DEFAULT,
     LBRACKET,
     LCURLY,
     LPARAN,
-    SINGLE_QUOTE,
-    DOUBLE_QUOTE,
-    BACK_QUOTE,
-} SIGN_ENUM;
+    RBRACKET,
+    RCURLY,
+    RPARAN
+} SYMBOL_ENUM;
 
-STACK_CREATE_INLINE_FUNCTIONS(se, SIGN_ENUM)
+STACK_CREATE_INLINE_FUNCTIONS(symb, SYMBOL_ENUM)
 
-char get_sign(SIGN_ENUM se) {
-    switch (se) {
+SYMBOL_ENUM encode_symbol(char c) {
+    switch (c) {
+    case '[':
+        return LBRACKET;
+    case '{':
+        return LBRACKET;
+    case '(':
+        return LPARAN;
+    case ']':
+        return RBRACKET;
+    case '}':
+        return RCURLY;
+    case ')':
+        return LPARAN;
+    default:
+        break;
+    }
+    return SYMBOL_ENUM_DEFAULT;
+}
+
+char decode_symbol(SYMBOL_ENUM symb) {
+    switch (symb) {
     case LBRACKET:
         return '[';
     case LCURLY:
         return '{';
     case LPARAN:
         return '(';
-    case SINGLE_QUOTE:
-        return '\'';
-    case DOUBLE_QUOTE:
-        return '"';
-    case BACK_QUOTE:
-        return '`';
-    case UNDEFINED_SIGN_ENUM:
+    case RBRACKET:
+        return ']';
+    case RCURLY:
+        return '}';
+    case RPARAN:
+        return ')';
+    default:
         break;
     }
-    return 'U';
+    return '\0';
 }
 
 int main(void) {
-    Stack* stack_p = stack_new_se();
-    if (stack_p == NULL) {
-        return 1;
-    }
-
     puts("Input line:");
 
     char* str = NULL;
@@ -49,77 +64,21 @@ int main(void) {
         return 1;
     }
 
-#ifdef DEBUG
-    putchar('\n');
-#endif
+    Stack* stack_p = stack_new_symb(n);
+    if (stack_p == NULL) {
+        free(str);
+        return 1;
+    }
 
     bool no_errors = true;
-    bool single_quote_used = false;
-    bool double_quote_used = false;
-    bool back_quote_used = false;
-    bool comment_sign_used = false;
     ssize_t i = 0;
-    int left_side;
+
     for (i = 0; i < n; i++) {
-        if (str[i] == '\\' && i + 1 < n) {
-            switch (str[i + 1]) {
-            case '/':
-            case '*':
-            case '(':
-            case ')':
-            case '[':
-            case ']':
-            case '{':
-            case '}':
-            case '"':
-            case '\'':
-            case '`':
-                i++;
-                continue;
-            }
-        }
-        switch (str[i]) {
-        case '/':
-            if (i + 1 < n && str[i + 1] == '*') {
-                if (!comment_sign_used) {
-                    comment_sign_used = true;
-                    i++; // skip next char
-                }
-            }
-            break;
-        case '*':
-            if (i + 1 < n && str[i + 1] == '/') {
-                if (comment_sign_used) {
-                    comment_sign_used = false;
-                    i++; // skip next char
-                } else {
-                    no_errors = false;
-                }
-            }
-            break;
-        default:
-            if (no_errors && comment_sign_used) {
-                continue; // continue loop without checking
-            }
-        }
         switch (str[i]) {
         case '(':
-            stack_push_se(stack_p, LPARAN);
-#ifdef DEBUG
-            printf("stack push: %c\n", str[i]);
-#endif
-            break;
         case '{':
-            stack_push_se(stack_p, LCURLY);
-#ifdef DEBUG
-            printf("stack push: %c\n", str[i]);
-#endif
-            break;
         case '[':
-            stack_push_se(stack_p, LBRACKET);
-#ifdef DEBUG
-            printf("stack push: %c\n", str[i]);
-#endif
+            stack_push_symb(stack_p, encode_symbol(str[i]));
             break;
         case ')':
         case '}':
@@ -128,12 +87,7 @@ int main(void) {
                 no_errors = false;
                 break;
             }
-            left_side = stack_pop_se(stack_p);
-#ifdef DEBUG
-            printf("stack pop: %c\n", get_sign(left_side));
-            printf("comparing %c with %c\n", get_sign(left_side), str[i]);
-#endif
-            switch (left_side) {
+            switch (stack_pop_symb(stack_p)) {
             case LPARAN:
                 no_errors = str[i] == ')';
                 break;
@@ -143,62 +97,8 @@ int main(void) {
             case LBRACKET:
                 no_errors = str[i] == ']';
                 break;
-            case SINGLE_QUOTE:
-            case DOUBLE_QUOTE:
-            case BACK_QUOTE:
-                no_errors = false;
+            default:
                 break;
-            }
-            break;
-        case '\'':
-            if (!single_quote_used) {
-                single_quote_used = true;
-#ifdef DEBUG
-                printf("stack push: %c\n", '\'');
-#endif
-                stack_push_se(stack_p, SINGLE_QUOTE);
-            } else {
-                left_side = stack_pop_se(stack_p);
-#ifdef DEBUG
-                printf("stack pop: %c\n", get_sign(left_side));
-                printf("comparing %c with %c\n", get_sign(left_side), str[i]);
-#endif
-                no_errors = left_side == SINGLE_QUOTE;
-                single_quote_used = false;
-            }
-            break;
-        case '"':
-            if (!double_quote_used) {
-                double_quote_used = true;
-#ifdef DEBUG
-                printf("stack push: %c\n", '"');
-#endif
-                stack_push_se(stack_p, DOUBLE_QUOTE);
-            } else {
-                left_side = stack_pop_se(stack_p);
-#ifdef DEBUG
-                printf("stack pop: %c\n", get_sign(left_side));
-                printf("comparing %c with %c\n", get_sign(left_side), str[i]);
-#endif
-                no_errors = left_side == DOUBLE_QUOTE;
-                double_quote_used = false;
-            }
-            break;
-        case '`':
-            if (!back_quote_used) {
-                back_quote_used = true;
-#ifdef DEBUG
-                printf("stack push: %c\n", '`');
-#endif
-                stack_push_se(stack_p, BACK_QUOTE);
-            } else {
-                left_side = stack_pop_se(stack_p);
-#ifdef DEBUG
-                printf("stack pop: %c\n", get_sign(left_side));
-                printf("comparing %c with %c\n", get_sign(left_side), str[i]);
-#endif
-                no_errors = left_side == BACK_QUOTE;
-                back_quote_used = false;
             }
             break;
         }
@@ -206,22 +106,20 @@ int main(void) {
             break;
         }
     }
-#ifdef DEBUG
-    printf("[result] no errors: %s, index stopped at: %lu\n", no_errors ? "true" : "false", i);
-#else
+
     if (no_errors) {
         printf("\nno errors : )\n");
     } else {
-        // these numbers are hand picked from testing and counting. just for
-        // pretty printing.
         if (i <= 8) {
             printf("%*c error at\n", (int)i + 1, '^');
         } else {
             printf("%*s ^\n", (int)i - 1, "error at");
         }
     }
-#endif
+
     stack_free(stack_p);
     free(str);
+
     return 0;
 }
+
