f83ea5e43927cc991b7e6ba6ec1a19943d06743d
Shamim <abir09bs@gmail.com>
2024-03-02 22:00:46 +0100

    add changes

--- implementations/dict/dict.c
+++ implementations/dict/dict.c
@@ -1,24 +1,9 @@
 #include <assert.h>  // assert
 #include <stdbool.h> // bool, true, false
 #include <stdint.h>  // SIZE_MAX
-#include <stdlib.h>  // size_t, malloc, free, reallocarray, NULL
-#include <string.h>  // memcpy, memcmp
+#include <stdlib.h>  // size_t, malloc, free, NULL
 
 #include "dict.h"
-
-#define FNV_OFFSET (0xcbf29ce484222325)
-#define FNV_PRIME (0x100000001b3)
-
-uint64_t hash(uint8_t* data, size_t data_size) {
-    // FNV-1a hash
-    // https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
-    uint64_t hash = FNV_OFFSET;
-    for (size_t i = 0; i < data_size; i++) {
-        hash ^= data[i];
-        hash *= FNV_PRIME;
-    }
-    return hash;
-}
 
 size_t rounduppow2(uint32_t v) {
     // https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
@@ -33,59 +18,69 @@ size_t rounduppow2(uint32_t v) {
     return v;
 }
 
-Dict* dict_new(size_t nelm, size_t key_size, size_t value_size) {
-    assert(nelm != 0);
+Dict* dict_new(size_t capacity, size_t key_size, size_t value_size, uint64_t (*key_get_hash)(unsigned char*, size_t),
+               bool (*key_isequal)(unsigned char*, unsigned char*)) {
+    assert(capacity != 0);
     assert(key_size != 0);
     assert(value_size != 0);
-    size_t capacity = rounduppow2(nelm * 1.25); // (*) 1.25 to keep load factor below 0.8
-    if (capacity < nelm || capacity > SIZE_MAX / sizeof(DictEntry) || capacity > SIZE_MAX / key_size ||
-        capacity > SIZE_MAX / value_size) {
+    size_t new_capacity = rounduppow2(capacity);
+    if (new_capacity < capacity || new_capacity > SIZE_MAX / sizeof(DictEntry) || new_capacity > SIZE_MAX / key_size ||
+        new_capacity > SIZE_MAX / value_size) {
         return NULL;
     }
     Dict* dict_p = malloc(sizeof(Dict));
     if (dict_p == NULL) {
         return NULL;
     }
-    dict_p->arr_p = malloc(capacity * sizeof(DictEntry));
-    if (dict_p->arr_p == NULL) {
+    dict_p->keys_arr_p = malloc(key_size * new_capacity);
+    if (dict_p->keys_arr_p == NULL) {
         free(dict_p);
         return NULL;
     }
-    dict_p->key_pool = malloc(capacity * key_size);
-    if (dict_p->arr_p == NULL) {
-        free(dict_p->arr_p);
+    dict_p->values_arr_p = malloc(value_size * new_capacity);
+    if (dict_p->values_arr_p == NULL) {
+        free(dict_p->values_arr_p);
         free(dict_p);
         return NULL;
     }
-    dict_p->value_pool = malloc(capacity * value_size);
-    if (dict_p->arr_p == NULL) {
-        free(dict_p->key_pool);
-        free(dict_p->arr_p);
+    dict_p->entries_arr_p = malloc(sizeof(DictEntry) * new_capacity);
+    if (dict_p->entries_arr_p == NULL) {
+        free(dict_p->keys_arr_p);
+        free(dict_p->values_arr_p);
         free(dict_p);
         return NULL;
     }
-    unsigned char* key_p = &dict_p->key_pool[0];
-    unsigned char* value_p = &dict_p->value_pool[0];
-    for (size_t i = 0; i < capacity; i++) {
-        dict_p->arr_p[i] = (DictEntry){.offset = -1, .key_p = key_p, .value_p = value_p};
-        key_p += key_size;
-        value_p += value_size;
+    for (size_t i = 0; i < new_capacity; i++) {
+        dict_p->entries_arr_p[i].offset = -1;
     }
-    dict_p->capacity_sub_one = capacity - 1;
+    dict_p->capacity_sub_one = new_capacity - 1;
     dict_p->used = 0;
+    dict_p->key_get_hash = key_get_hash;
+    dict_p->key_isequal = key_isequal;
     dict_p->key_size = key_size;
     dict_p->value_size = value_size;
     return dict_p;
 }
 
+bool dict_exists(Dict* dict_p, unsigned char* key_p) {
+    return false;
+}
 
+unsigned char* dict_get(Dict* dict_p, unsigned char* key_p) {
+    return NULL;
+}
+
+bool dict_set(Dict* dict_p, unsigned char* key_p, unsigned char* value_p) {
+    return false;
+}
 
 void dict_free(Dict** dict_pp) {
     if (*dict_pp == NULL) {
         return;
     }
-    free((*dict_pp)->key_pool);
-    free((*dict_pp)->value_pool);
-    free((*dict_pp)->arr_p);
+    free((*dict_pp)->entries_arr_p);
+    free((*dict_pp)->keys_arr_p);
+    free((*dict_pp)->values_arr_p);
     free((*dict_pp));
+    *dict_pp = NULL;
 }
--- implementations/dict/dict.h
+++ implementations/dict/dict.h
@@ -1,29 +1,37 @@
 #pragma once
 
-#include <assert.h>  // static_assert
 #include <stdbool.h> // bool
-#include <stdint.h>  // hash
+#include <stdint.h>  // uint64_t
 #include <stdlib.h>  // size_t
 
-typedef struct DictEntry {
-    unsigned char* key_p;
-    void* value_p;
+typedef struct {
+    uint64_t offset;
+    uint64_t hash;
+    uint64_t index;
 } DictEntry;
 
 typedef struct {
     size_t capacity_sub_one;
-    size_t used_slots;
+    size_t used;
+    DictEntry* entries_arr_p;
+
+    uint64_t (*key_get_hash)(unsigned char*, size_t);
+    bool (*key_isequal)(unsigned char*, unsigned char*);
+
     size_t key_size;
-    uint64_t* indicies;
+    size_t value_size;
+    unsigned char* keys_arr_p;
+    unsigned char* values_arr_p;
 } Dict;
 
-Dict* dict_new(size_t initial_capacity, size_t key_size);
+Dict* dict_new(size_t capacity, size_t key_size, size_t value_size, uint64_t (*key_get_hash)(unsigned char*, size_t),
+               bool (*key_isequal)(unsigned char*, unsigned char*));
 
 bool dict_exists(Dict* dict_p, unsigned char* key_p);
 
-void* dict_get(Dict* dict_p, unsigned char* key_p);
+unsigned char* dict_get(Dict* dict_p, unsigned char* key_p);
 
-bool dict_set(Dict* dict_p, unsigned char* key_p, void* value_p, size_t value_size);
+bool dict_set(Dict* dict_p, unsigned char* key_p, unsigned char* value_p);
 
 bool dict_del(Dict* dict_p, unsigned char* key_p);
 
