4a1957cc19a7d7264e0befd91b98b48a524afb70
Shamim <abir09bs@gmail.com>
2024-03-08 02:20:14 +0100

    change script

---
+++ tests/build_and_run_tests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os, sys, glob, pathlib
+
+bin_dir = "../bin"
+include_dir = "../src"
+
+os.chdir(sys.path[0])
+
+if not os.path.exists(bin_dir):
+    os.makedirs(bin_dir)
+
+for name in glob.glob("./*_tests.c"):
+    s = pathlib.Path(name).stem
+    os.system(f"gcc -Wall -Wextra -I{include_dir} -o {bin_dir}/{s}.bin ../src/*.c {s}.c")
+
+os.system("find ../bin/ -type f -exec '{}' ';'")
