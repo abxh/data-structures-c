48f060e5b4445915ba94dec5c9a15deed711f81b
Shamim <abir09bs@gmail.com>
2024-03-15 19:54:54 +0100

    slight renaming

--- src/queue/queue_helpers.h
+++ src/queue/queue_helpers.h
@@ -3,8 +3,8 @@
     which can be used to directly interact with values.
 
     The functions generated for a given value type t with label T are:
-    - queuenode_get_value_T
-    - queuenode_set_value_T
+    - queuenode_get_value_as_T
+    - queuenode_set_value_to_T
     - queue_peek_T
     - queue_push_T
     - queue_pop_T
@@ -31,21 +31,21 @@
 #define PASTE(a, b) CAT(a, b)
 #define JOIN(prefix, name) PASTE(prefix, PASTE(_, name))
 
-#define queuenode_get_value_T JOIN(queuenode_get_value, VALUE_LABEL)
-#define queuenode_set_value_T JOIN(queuenode_set_value, VALUE_LABEL)
+#define queuenode_get_value_as_T JOIN(queuenode_get_value_as, VALUE_LABEL)
+#define queuenode_set_value_to_T JOIN(queuenode_set_value_to, VALUE_LABEL)
 #define queue_peek_T JOIN(queue_peek, VALUE_LABEL)
 #define queue_peek_first_T JOIN(queue_peek_first, VALUE_LABEL)
 #define queue_peek_last_T JOIN(queue_peek_last, VALUE_LABEL)
 #define queue_enqueue_T JOIN(queue_enqueue, VALUE_LABEL)
 #define queue_dequeue_T JOIN(queue_dequeue, VALUE_LABEL)
 
-static inline VALUE_TYPE queuenode_get_value_T(const QueueNode* node_p) {
+static inline VALUE_TYPE queuenode_get_value_as_T(const QueueNode* node_p) {
     assert(node_p != NULL);
 
     return *(VALUE_TYPE*)node_p->value_p;
 }
 
-static inline void queuenode_set_value_T(QueueNode* node_p, VALUE_TYPE value) {
+static inline void queuenode_set_value_to_T(QueueNode* node_p, VALUE_TYPE value) {
     assert(node_p != NULL);
 
     *(VALUE_TYPE*)node_p->value_p = value;
@@ -55,21 +55,21 @@ static inline VALUE_TYPE queue_peek_T(const Queue* queue_p) {
     assert(queue_p != NULL);
     assert(queue_p->value_size == sizeof(VALUE_TYPE));
 
-    return queuenode_get_value_T(queue_peek_node(queue_p));
+    return queuenode_get_value_as_T(queue_peek_node(queue_p));
 }
 
 static inline VALUE_TYPE queue_peek_first_T(const Queue* queue_p) {
     assert(queue_p != NULL);
     assert(queue_p->value_size == sizeof(VALUE_TYPE));
 
-    return queuenode_get_value_T(queue_peek_first_node(queue_p));
+    return queuenode_get_value_as_T(queue_peek_first_node(queue_p));
 }
 
 static inline VALUE_TYPE queue_peek_last_T(const Queue* queue_p) {
     assert(queue_p != NULL);
     assert(queue_p->value_size == sizeof(VALUE_TYPE));
 
-    return queuenode_get_value_T(queue_peek_last_node(queue_p));
+    return queuenode_get_value_as_T(queue_peek_last_node(queue_p));
 }
 
 static inline bool queue_enqueue_T(Queue* queue_p, VALUE_TYPE value) {
@@ -85,7 +85,7 @@ static inline bool queue_enqueue_T(Queue* queue_p, VALUE_TYPE value) {
         free(node_p);
         return false;
     }
-    queuenode_set_value_T(node_p, value);
+    queuenode_set_value_to_T(node_p, value);
 
     queue_enqueue_node(queue_p, node_p);
 
@@ -99,7 +99,7 @@ static inline VALUE_TYPE queue_dequeue_T(Queue* queue_p) {
     QueueNode* node_p = queue_dequeue_node(queue_p);
     void* value_p = node_p->value_p;
 
-    VALUE_TYPE value = queuenode_get_value_T(node_p);
+    VALUE_TYPE value = queuenode_get_value_as_T(node_p);
 
     free(value_p);
     free(node_p);
@@ -107,8 +107,8 @@ static inline VALUE_TYPE queue_dequeue_T(Queue* queue_p) {
     return value;
 }
 
-#undef queuenode_get_value_T
-#undef queuenode_set_value_T
+#undef queuenode_get_value_as_T
+#undef queuenode_set_value_to_T
 #undef queue_peek_T
 #undef queue_peek_first_T
 #undef queue_peek_last_T
--- src/stack/stack_helpers.h
+++ src/stack/stack_helpers.h
@@ -3,8 +3,8 @@
     which can be used to directly interact with values.
 
     The functions generated for a given value type t with label T are:
-    - stacknode_get_value_T
-    - stacknode_set_value_T
+    - stacknode_get_value_as_T
+    - stacknode_set_value_to_T
     - stack_peek_T
     - stack_push_T
     - stack_pop_T
@@ -31,19 +31,19 @@
 #define PASTE(a, b) CAT(a, b)
 #define JOIN(prefix, name) PASTE(prefix, PASTE(_, name))
 
-#define stacknode_get_value_T JOIN(stacknode_get_value, VALUE_LABEL)
-#define stacknode_set_value_T JOIN(stacknode_set_value, VALUE_LABEL)
+#define stacknode_get_value_as_T JOIN(stacknode_get_value_as, VALUE_LABEL)
+#define stacknode_set_value_to_T JOIN(stacknode_set_value_as, VALUE_LABEL)
 #define stack_peek_T JOIN(stack_peek, VALUE_LABEL)
 #define stack_push_T JOIN(stack_push, VALUE_LABEL)
 #define stack_pop_T JOIN(stack_pop, VALUE_LABEL)
 
-static inline VALUE_TYPE stacknode_get_value_T(const StackNode* node_p) {
+static inline VALUE_TYPE stacknode_get_value_as_T(const StackNode* node_p) {
     assert(node_p != NULL);
 
     return *(VALUE_TYPE*)node_p->value_p;
 }
 
-static inline void stacknode_set_value_T(StackNode* node_p, VALUE_TYPE value) {
+static inline void stacknode_set_value_to_T(StackNode* node_p, VALUE_TYPE value) {
     assert(node_p != NULL);
 
     *(VALUE_TYPE*)node_p->value_p = value;
@@ -53,7 +53,7 @@ static inline VALUE_TYPE stack_peek_T(const Stack* stack_p) {
     assert(stack_p != NULL);
     assert(stack_p->value_size == sizeof(VALUE_TYPE));
 
-    return stacknode_get_value_T(stack_peek_node(stack_p));
+    return stacknode_get_value_as_T(stack_peek_node(stack_p));
 }
 
 static inline bool stack_push_T(Stack* stack_p, VALUE_TYPE value) {
@@ -69,7 +69,7 @@ static inline bool stack_push_T(Stack* stack_p, VALUE_TYPE value) {
         free(node_p);
         return false;
     }
-    stacknode_set_value_T(node_p, value);
+    stacknode_set_value_to_T(node_p, value);
 
     stack_push_node(stack_p, node_p);
 
@@ -83,7 +83,7 @@ static inline VALUE_TYPE stack_pop_T(Stack* stack_p) {
     StackNode* node_p = stack_pop_node(stack_p);
     void* value_p = node_p->value_p;
 
-    VALUE_TYPE value = stacknode_get_value_T(node_p);
+    VALUE_TYPE value = stacknode_get_value_as_T(node_p);
 
     free(value_p);
     free(node_p);
@@ -91,8 +91,8 @@ static inline VALUE_TYPE stack_pop_T(Stack* stack_p) {
     return value;
 }
 
-#undef stacknode_get_value_T
-#undef stacknode_set_value_T
+#undef stacknode_get_value_as_T
+#undef stacknode_set_value_to_T
 #undef stack_peek_T
 #undef stack_push_T
 #undef stack_pop_T
