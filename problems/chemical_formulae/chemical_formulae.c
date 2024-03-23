16f37c5c3237e8b1a7e92d6e6bda95595d738f42
Shamim <abir09bs@gmail.com>
2024-03-23 06:44:01 +0100

    use longest common subsequence

--- problems/chemical_formulae/chemical_formulae.c
+++ problems/chemical_formulae/chemical_formulae.c
@@ -2,38 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <wctype.h>
 
 #include "strmap.h"
 
-#define MIN3(a, b, c) ((a) < (b) ? ((a) < (c) ? (a) : (c)) : ((b) < (c) ? (b) : (c)))
+#include <stdio.h>
+#include <stdlib.h>
 
-int levenshtein(char* s1, char* s2) {
-    // Levenshtein distance - a measure of how similar two strings are.
-    // https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#C
+#define max(x, y) ((x) > (y) ? (x) : (y))
 
-    unsigned int s1len, s2len, x, y, lastdiag, olddiag;
-    s1len = strlen(s1);
-    s2len = strlen(s2);
-    unsigned int column[s1len + 1];
-    for (y = 1; y <= s1len; y++)
-        column[y] = y;
-    for (x = 1; x <= s2len; x++) {
-        column[0] = x;
-        for (y = 1, lastdiag = x - 1; y <= s1len; y++) {
-            olddiag = column[y];
-            column[y] = MIN3(column[y] + 1, column[y - 1] + 1, lastdiag + (s1[y - 1] == s2[x - 1] ? 0 : 1));
-            lastdiag = olddiag;
+#define line_len_max 150
+static uint64_t result[line_len_max][line_len_max];
+
+uint64_t longest_common_subsequence(const char* str1, size_t n, const char* str2, size_t m) {
+    // https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_subsequence
+    memset(result, 0, sizeof(result));
+    for (size_t i = 1; i <= n; i++) {
+        for (size_t j = 1; j <= m; j++) {
+            if (tolower(str1[i - 1]) == tolower(str2[j - 1])) {
+                result[i][j] = result[i - 1][j - 1] + 1;
+            } else {
+                result[i][j] = max(result[i - 1][j], result[i][j - 1]);
+            }
         }
     }
-    return column[s1len];
+    return result[n][m];
 }
+
+#define abs_diff(x, y) ((x) >= (y) ? (x) - (y) : (y) - (x))
 
 static char* CMP_CUSTOM_USR_INP_P = NULL;
 int cmp_custom(const void* s1, const void* s2) {
-    int dist_for_s1 = levenshtein(CMP_CUSTOM_USR_INP_P, *(char**)s1);
-    int dist_for_s2 = levenshtein(CMP_CUSTOM_USR_INP_P, *(char**)s2);
-    return -(dist_for_s1 < dist_for_s2) + (dist_for_s1 > dist_for_s2);
+    size_t n = strlen(CMP_CUSTOM_USR_INP_P);
+    size_t m1 = strlen(*(char**)s1);
+    size_t m2 = strlen(*(char**)s2);
+
+    uint64_t lcs1 = longest_common_subsequence(CMP_CUSTOM_USR_INP_P, n, *(char**)s1, m1);
+    uint64_t lcs2 = longest_common_subsequence(CMP_CUSTOM_USR_INP_P, n, *(char**)s2, m2);
+
+    if (lcs1 != lcs2) {
+        return -(lcs1 < lcs2) + (lcs1 > lcs2);
+    }
+
+    size_t diff1 = abs_diff(m1, n);
+    size_t diff2 = abs_diff(m2, n);
+
+    return (diff2 > diff1) - (diff2 < diff1);
 }
 
 int main(void) {
@@ -57,7 +70,6 @@ int main(void) {
 
     // read from file and store data as keys and values in strmap:
     size_t lines = 0;
-    size_t line_len_max = 150;
     while (true) {
         if (lines == 0) {
             lines++;
@@ -93,7 +105,7 @@ int main(void) {
             *iter_str = towlower(*iter_str);
         }
 
-        char buf1[line_len_max];
+        char buf1[line_len_max * 2];
         sprintf(buf1, "%s%s%s", formula_str, *cas_str != '\0' ? ", CAS: " : "", cas_str);
         strmap_set(strmap_p,
                    synonym_str, // key
@@ -102,7 +114,6 @@ int main(void) {
 
         lines++;
     }
-    printf("actual: %lu why?:%lu",lines, strmap_p->list_count);
 
     // create an array of key references:
     char** keys_arr_pp = malloc(sizeof(char*) * lines);
@@ -130,7 +141,7 @@ int main(void) {
     while (0 < (len = getline(&line_p, &n, stdin))) {
         CMP_CUSTOM_USR_INP_P = &line_p[0];
         qsort(keys_arr_pp, lines, sizeof(char*), cmp_custom);
-        for (size_t i = 0; i < 5; i++) {
+        for (size_t i = lines - 1; i >= lines - 5; i--) {
             printf(" %s\n  -> %s\n", keys_arr_pp[i], strmap_get(strmap_p, keys_arr_pp[i]));
         }
     }
