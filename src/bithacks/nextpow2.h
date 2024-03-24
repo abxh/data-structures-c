caafac63d302623d12beb403dfcc8b38154058af
Shamim <abir09bs@gmail.com>
2024-03-24 17:39:29 +0100

    add nextpow2

--- src/bithacks/nextpow2.h
+++ src/bithacks/nextpow2.h
@@ -1,52 +1,38 @@
 
 #include <assert.h> // assert
-#include <stdint.h> // uint32_t, uint64_t
+#include <stdint.h> // INTPTR_MAX, INT32_MAX, INT64_MAX
+#include <stdlib.h> // abort
+#include <stdlib.h> // size_t
 
 // Reference:
 // https://stackoverflow.com/questions/466204/rounding-up-to-next-power-of-2
-
-static inline uint32_t next_pow2_u32_portable(uint32_t x) {
-    assert(x != 0);
-    x--;
-    x |= x >> 1;
-    x |= x >> 2;
-    x |= x >> 4;
-    x |= x >> 8;
-    x |= x >> 16;
-    x++;
-    return x;
-}
-
-#if __x86_64__ // Test for 64-bit support
-static inline uint64_t next_pow2_u64_portable(uint64_t x) {
-    assert(x != 0);
-    x--;
-    x |= x >> 1;
-    x |= x >> 2;
-    x |= x >> 4;
-    x |= x >> 8;
-    x |= x >> 16;
-    x |= x >> 32;
-    x++;
-    return x;
-}
-#endif
-
-// See the following for info on builtin instructions:
 // https://en.wikipedia.org/wiki/Find_first_set#Tool_and_library_support
+// https://stackoverflow.com/questions/5272825/detecting-64bit-compile-in-c
+
+static inline size_t next_pow2(size_t x) {
+    assert(x != 0);
 
 #if __GNUC__ > 3 || (__GNUC__ == 3 && (__GNUC_MINOR__ > 4 || __GNUC_MINOR__ == 4)) // Test for GCC >= 3.4.0
-static inline uint32_t next_pow2_u32_gcc_builtin(uint32_t x) {
-    assert(x > 1);
-    return 1 << (32 - __builtin_clz(x - 1));
-}
+#if UINTPTR_MAX == UINT32_MAX
+    return x == 1 ? 1 : 1 << (32 - __builtin_clz(x - 1));
+#elif UINTPTR_MAX == UINT64_MAX
+    return x == 1 ? 1 : 1 << (64 - __builtin_clzl(x - 1));
+#endif
 #endif
 
-#if __x86_64__ // Test for 64-bit support
-#if __GNUC__ > 3 || (__GNUC__ == 3 && (__GNUC_MINOR__ > 4 || __GNUC_MINOR__ == 4)) // Test for GCC >= 3.4.0
-static inline uint64_t next_pow2_u64_gcc_builtin(uint64_t x) {
-    assert(x > 1);
-    return 1 << (64 - __builtin_clzl(x - 1));
+#if UINTPTR_MAX == UINT32_MAX || UINTPTR_MAX == UINT64_MAX
+    x--;
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+#if UINTPTR_MAX == UINT64_MAX
+    x |= x >> 32;
+#endif
+    x++;
+    return x;
+#else
+    abort();
+#endif
 }
-#endif
-#endif
