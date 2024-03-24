ee7114cfb1f7d5adb630339de62d7d81a6136f22
Shamim <abir09bs@gmail.com>
2024-03-24 23:48:32 +0100

    renaming

--- tests/eval_and_log.h
+++ tests/eval_and_log.h
@@ -3,11 +3,11 @@
 // reference:
 // https://stackoverflow.com/a/168611
 
-static inline bool test_internal(bool expr_value, int linenr, const char* pretty_function, const char* expr_str) {
+static inline bool eval_and_log_internal(bool expr_value, int linenr, const char* pretty_function, const char* expr_str) {
     if (!expr_value) {
         fprintf(stderr, "line %d (%s): precondition `%s' failed.\n", linenr, pretty_function, expr_str);
     }
     return expr_value;
 }
 
-#define eval_and_log(expr) test_internal((expr), __LINE__, __PRETTY_FUNCTION__, #expr)
+#define eval_and_log(expr) eval_and_log_internal((expr), __LINE__, __PRETTY_FUNCTION__, #expr)
