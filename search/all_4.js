e2fdc6150b39194102d242bd72aa076f6edfa36b
abxh <abxh@users.noreply.github.com>
2024-04-25 21:16:35 +0000

    deploy: daf49dfdb904df3f5b78a338f8e4938bf5848c0d

--- search/all_4.js
+++ search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['paste_0',['PASTE',['../join_8h.html#a188ea780417dce4eb720d161d85f9f44',1,'join.h']]]
+  ['paste_0',['PASTE',['../join_8h.html#a188ea780417dce4eb720d161d85f9f44',1,'join.h']]],
+  ['prefix_1',['PREFIX',['../stack_8h.html#add51b53e8b628f95078327028dccb20c',1,'stack.h']]]
 ];
