6db33cf9e9f4267970a01e202481bce41dcc6d25
abxh <83485102+abxh@users.noreply.github.com>
2024-04-26 15:09:10 +0200

    [minor] improve docs

--- src/std_allocator.h
+++ src/std_allocator.h
@@ -1,7 +1,6 @@
 /**
  * @file std_allocator.h
- * @brief wrapper of std functions for conforming to interface of allocator operations.
- * @note NULL can be passed as context_ptr, which is not used by the std functions.
+ * @brief wrapper of std functions for conforming to the defined interface of allocator operations.
  */
 
 #pragma once
@@ -12,14 +11,53 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
+/**
+ * @brief `malloc` wrapper to allocate memory.
+ * @note Can pass NULL as context\_ptr.
+ *
+ * @param[in] context\_ptr (unused)
+ * @param[in] alignment (unused)
+ * @param[in] size Amount of memory to allocate in bytes.
+ * @return The stack pointer.
+ * @retval NULL
+ * - Implementation-defined, but may do so if initial\_capacity is 0.
+ * - If no memory space is available.
+ */
 static inline void* std_allocate(void* context_ptr, size_t alignment, size_t size) {
     return malloc(size);
 }
-static inline void* std_reallocate(void* context_ptr, void* ptr, size_t alignment, size_t old_size, size_t new_size) {
-    return realloc(ptr, new_size);
+
+/**
+ * @brief `realloc` wrapper to allocate memory.
+ * @note Can pass NULL as context\_ptr.
+ *
+ * @warning Passing memory_ptr not allocated by malloc, calloc or free is UB.
+ *
+ * @param[in] context\_ptr (unused)
+ * @param[in] mem\_ptr Pointer to the memory buffer. If NULL, then malloc(size) is called.
+ * @param[in] alignment (unused)
+ * @param[in] old_size (unused)
+ * @param[in] new_size Amount of memory to allocate in bytes.
+ * @return A new pointer, pointing to a new\_size-sized buffer with old contents from the buffer at mem\_ptr.
+ * @retval NULL
+ * - Implementation-defined, but may do so if initial\_capacity is 0.
+ * - If no memory space is available.
+ */
+static inline void* std_reallocate(void* context_ptr, void* mem_ptr, size_t alignment, size_t old_size, size_t new_size) {
+    return realloc(mem_ptr, new_size);
 }
-static inline void std_deallocate(void* context_ptr, void* ptr) {
-    return free(ptr);
+
+/**
+ * @brief `free` wrapper to allocate memory.
+ * @note Can pass NULL as context\_ptr.
+ *
+ * @warning May not free the same allocated buffer twice, as this may lead to memory leaks.
+ *
+ * @param[in] context_ptr (unused)
+ * @param[in] mem\_ptr Pointer to the memory buffer. If NULL, then no operation is done.
+ */
+static inline void std_deallocate(void* context_ptr, void* mem_ptr) {
+    return free(mem_ptr);
 }
 
 #pragma GCC diagnostic pop
