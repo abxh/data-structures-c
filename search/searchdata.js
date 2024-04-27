af7923fa2c32c665f496770bb12f042fef81eea2
abxh <abxh@users.noreply.github.com>
2024-04-27 19:53:12 +0000

    deploy: 8713660fdb73d5bb7e029dc96b4f5eac16baef30

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,11 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "acdeijpqrstv",
+  0: "acdeijlpqrstv",
   1: "at",
   2: "aijqrs",
   3: "irst",
   4: "acdeirsv",
-  5: "cjpqsv"
+  5: "cjpqsv",
+  6: "lt"
 };
 
 var indexSectionNames =
@@ -15,7 +16,8 @@ var indexSectionNames =
   2: "files",
   3: "functions",
   4: "variables",
-  5: "defines"
+  5: "defines",
+  6: "pages"
 };
 
 var indexSectionLabels =
@@ -25,6 +27,7 @@ var indexSectionLabels =
   2: "Files",
   3: "Functions",
   4: "Variables",
-  5: "Macros"
+  5: "Macros",
+  6: "Pages"
 };
 
