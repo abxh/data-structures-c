e1481fe906c37eb1dc46cc515837b4e46a116cfa
Shamim <abir09bs@gmail.com>
2024-07-15 16:40:13 +0200

    add work in progress

--- lib/list.h
+++ lib/list.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include "container_of.h" // container_of
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -21,264 +23,98 @@ typedef struct list_node_type {
 } list_node_type;
 
 /**
- * @brief List structure, keeping track of head nod, tail node and node count.
+ * @def list_node_entry(node_ptr, container_type, node_member_name)
+ * @brief Get the pointer to the container instance, given a pointer to
+ *        to the node member instance, container type, and node member name.
+ *
+ * @param[in] node_ptr A pointer to the node member.
+ * @param[in] container_type The container type.
+ * @param[in] node_member_name The node member name.
+ * @retval A pointer to the container instance.
  */
-typedef struct {
-    struct list_node_type* head_ptr; ///< head node pointer.
-    struct list_node_type* tail_ptr; ///< tail node pointer.
-    size_t count;                    ///< Node count.
-} list_type;
+#define list_node_entry(node_ptr, container_type, node_member_name) container_of(node_ptr, container_type, node_member_name)
 
 /**
- * @brief Initalize a list.
- * @note The list could heap / stack allocated depending on program structure.
+ * @brief Initialize a list node
  *
- * Assumes list_ptr is not NULL.
- *
- * @param[in] list_ptr The list pointer.
- */
-static inline void list_init(list_type* list_ptr) {
-    assert(list_ptr != NULL);
-
-    list_ptr->head_ptr = NULL;
-    list_ptr->tail_ptr = NULL;
-    list_ptr->count = 0;
-}
-
-/**
- * @brief Initalize a list node.
- * @note The list node could heap / stack allocated depending on program structure.
- *
- * Assumes node_ptr is not NULL.
- *
- * @param[in] node_ptr The list pointer.
+ * @param[in,out] node_ptr The node pointer.
  */
 static inline void list_node_init(list_node_type* node_ptr) {
-    assert(node_ptr != NULL);
-
-    node_ptr->prev_ptr = NULL;
-    node_ptr->next_ptr = NULL;
+    node_ptr->prev_ptr = node_ptr->next_ptr = node_ptr;
 }
 
 /**
- * @brief Check if the list empty.
- *
- * Assumes list_ptr is not NULL.
- *
- * @param[in] list_ptr The list pointer.
- */
-static inline bool list_is_empty(list_type* list_ptr) {
-    assert(list_ptr != NULL);
-
-    return list_ptr->head_ptr == NULL; // or list_ptr->tail_ptr == NULL
-}
-
-/**
- * @brief Check if the list node is head.
+ * @brief Check if a given list node is first in the list.
  *
  * @param[in] node_ptr The node pointer.
- * @return Whether list node is head.
+ *
+ * @return Whether the list node is first in the list.
  */
-static inline bool list_node_is_head(list_node_type* node_ptr) {
-    assert(node_ptr != NULL);
-
-    return node_ptr->prev_ptr == NULL;
+static inline bool list_node_is_first(const list_node_type* node_ptr) {
+    return node_ptr->prev_ptr == node_ptr;
 }
 
 /**
- * @brief Check if the list node is tail.
+ * @brief Check if a given list node is last in the list.
  *
  * @param[in] node_ptr The node pointer.
- * @return Whether list node is tail.
+ *
+ * @return Whether the list node is last in the list.
  */
-static inline bool list_node_is_tail(list_node_type* node_ptr) {
-    assert(node_ptr != NULL);
-
-    return node_ptr->next_ptr == NULL;
+static inline bool list_node_is_last(const list_node_type* node_ptr) {
+    return node_ptr->next_ptr == node_ptr;
 }
 
 /**
- * @brief Append a node to the head of the list.
+ * @brief Count number of nodes after a given node.
  *
- * Assumes:
- * @li list_ptr is not NULL.
- * @li node_ptr is not NULL.
+ * This does not count the given node itself. So if the given node is the last node,
+ * the function returns 0.
  *
- * @param[in] list_ptr The list pointer.
+ * @param[in] head_ptr The head node pointer.
+ *
+ * @return Number of nodes *after* head_ptr.
+ *  @retval 0 If the given node is the last node.
+ */
+static inline size_t list_node_count_after(list_node_type* head_ptr) {
+    list_node_type* node_ptr = head_ptr;
+    size_t count = 0;
+
+    while (!list_node_is_last(node_ptr)) {
+        node_ptr = node_ptr->next_ptr;
+        count++;
+    }
+    return count;
+}
+
+/// @cond DO_NOT_DOCUMENT
+static inline void internal_list_node_add_between(list_node_type* node_ptr, list_node_type* before_ptr, list_node_type* after_ptr) {
+    before_ptr->next_ptr = node_ptr;
+    node_ptr->prev_ptr = before_ptr;
+
+    after_ptr->prev_ptr = node_ptr;
+    node_ptr->next_ptr = after_ptr;
+}
+/// @endcond
+
+/**
+ * @brief Add a node *after* the head node.
+ *
+ * @param[in] head_ptr The head node pointer.
  * @param[in] node_ptr The node pointer.
  */
-static inline void list_appendleft(list_type* list_ptr, list_node_type* node_ptr) {
-    assert(list_ptr != NULL);
-    assert(node_ptr != NULL);
-
-    list_node_init(node_ptr);
-
-    if (list_is_empty(list_ptr)) {
-        list_ptr->head_ptr = node_ptr;
-        list_ptr->tail_ptr = node_ptr;
-    } else {
-        list_ptr->head_ptr->prev_ptr = node_ptr;
-        node_ptr->next_ptr = list_ptr->head_ptr;
-        list_ptr->head_ptr = node_ptr;
-    }
-
-    list_ptr->count++;
+static inline void list_add_after(list_node_type* node_ptr, list_node_type* head_ptr) {
+    internal_list_node_add_between(node_ptr, head_ptr, head_ptr->next_ptr);
 }
 
 /**
- * @brief Append a node to the tail of the list.
+ * @brief Add a node *before* the tail node.
  *
- * Assumes:
- * @li list_ptr is not NULL.
- * @li node_ptr is not NULL.
- *
- * @param[in] list_ptr The list pointer.
+ * @param[in] tail_ptr The tail node pointer.
  * @param[in] node_ptr The node pointer.
  */
-static inline void list_append(list_type* list_ptr, list_node_type* node_ptr) {
-    assert(list_ptr != NULL);
-    assert(node_ptr != NULL);
-
-    list_node_init(node_ptr);
-
-    if (list_is_empty(list_ptr)) {
-        list_ptr->head_ptr = node_ptr;
-        list_ptr->tail_ptr = node_ptr;
-    } else {
-        list_ptr->tail_ptr->next_ptr = node_ptr;
-        node_ptr->prev_ptr = list_ptr->tail_ptr;
-        list_ptr->tail_ptr = node_ptr;
-    }
-
-    list_ptr->count++;
-}
-
-/**
- * @brief Pop the tail of the list.
- *
- * Assumes list_ptr is not NULL.
- *
- * @param[in] list_ptr The list pointer.
- * @return The node pointer.
- *  @retval NULL if the list is empty.
- */
-static inline list_node_type* list_pop(list_type* list_ptr) {
-    assert(list_ptr != NULL);
-
-    if (list_is_empty(list_ptr)) {
-        return NULL;
-    }
-
-    list_node_type* node_ptr = list_ptr->tail_ptr;
-
-    list_ptr->tail_ptr = list_ptr->tail_ptr->prev_ptr;
-    if (list_ptr->tail_ptr == NULL) {
-        list_ptr->head_ptr = NULL;
-    } else {
-        list_ptr->tail_ptr->next_ptr = NULL;
-    }
-    list_ptr->count--;
-
-    return node_ptr;
-}
-
-/**
- * @brief Pop the head of the list.
- *
- * Assumes:
- * @li list_ptr is not NULL.
- *
- * @param[in] list_ptr The list pointer.
- * @return The node pointer.
- *  @retval NULL if the list is empty.
- */
-static inline list_node_type* list_popleft(list_type* list_ptr) {
-    assert(list_ptr != NULL);
-
-    if (list_is_empty(list_ptr)) {
-        return NULL;
-    }
-
-    list_node_type* node_ptr = list_ptr->head_ptr;
-
-    list_ptr->head_ptr = list_ptr->head_ptr->next_ptr;
-    if (list_ptr->head_ptr == NULL) {
-        list_ptr->tail_ptr = NULL;
-    } else {
-        list_ptr->head_ptr->prev_ptr = NULL;
-    }
-    list_ptr->count--;
-
-    return node_ptr;
-}
-
-/**
- * @brief Erase a given node from the list.
- *
- * Assumes node_ptr is not NULL.
- *
- * @param[in] list_ptr The list pointer.
- * @param[in] node_ptr The node pointer.
- */
-static inline void list_erase(list_type* list_ptr, list_node_type* node_ptr) {
-    assert(list_ptr != NULL);
-    assert(node_ptr != NULL);
-
-    if (list_node_is_head(node_ptr)) {
-        list_popleft(list_ptr);
-        return;
-    } else if (list_node_is_tail(node_ptr)) {
-        list_pop(list_ptr);
-        return;
-    }
-
-    assert(list_ptr->count > 2);
-
-    node_ptr->prev_ptr->next_ptr = node_ptr->next_ptr;
-    node_ptr->next_ptr->prev_ptr = node_ptr->prev_ptr;
-
-    list_node_init(node_ptr);
-}
-
-/**
- * @brief Swap a given node with another node in the list.
- *
- * Assumes both node points are not NULL.
- *
- * @param[in] list_ptr The list pointer.
- * @param[in] a_ptr Pointer to the first node.
- * @param[in] b_ptr Pointer to the second node.
- */
-static inline void list_swap(list_type* list_ptr, list_node_type* a_ptr, list_node_type* b_ptr) {
-    assert(a_ptr != NULL);
-    assert(b_ptr != NULL);
-
-    if (list_node_is_head(a_ptr)) {
-        assert(!list_node_is_head(b_ptr));
-
-        list_ptr->head_ptr = b_ptr;
-    } else if (list_node_is_head(b_ptr)) {
-        assert(!list_node_is_head(a_ptr));
-
-        list_ptr->head_ptr = a_ptr;
-    }
-    if (list_node_is_tail(a_ptr)) {
-        assert(!list_node_is_tail(b_ptr));
-        
-        list_ptr->tail_ptr = a_ptr;
-    } else if (list_node_is_tail(b_ptr)) {
-        assert(!list_node_is_tail(a_ptr));
-        
-        list_ptr->tail_ptr = b_ptr;
-    }
-
-    list_node_type* prev_ptr = a_ptr->prev_ptr;
-    a_ptr->prev_ptr = b_ptr->prev_ptr;
-    b_ptr->prev_ptr = prev_ptr;
-
-    list_node_type* next_ptr = a_ptr->next_ptr;
-    a_ptr->next_ptr = b_ptr->next_ptr;
-    b_ptr->next_ptr = next_ptr;
+static inline void list_add_before(list_node_type* node_ptr, list_node_type* tail_ptr) {
+    internal_list_node_add_between(node_ptr, tail_ptr->prev_ptr, tail_ptr);
 }
 
 // vim: ft=c fdm=marker
