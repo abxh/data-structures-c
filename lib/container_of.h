7f3978f1deb643f488e3eea3fb85c629a580ac7d
Shamim <abir09bs@gmail.com>
2024-07-15 16:40:13 +0200

    add work in progress

--- lib/container_of.h
+++ lib/container_of.h
@@ -13,12 +13,11 @@
  * @brief Get the pointer to the container instance, given a pointer to
  *        to the member instance, container type, and member name.
  *
- * @param[in] ptr The pointer to the buffer the container is stored in.
+ * @param[in] ptr A pointer to the member.
  * @param[in] type The container type.
  * @param[in] member The member name.
  * @retval A pointer to the container instance.
  */
-
 #define container_of(ptr, type, member)                   \
     __extension__({                                       \
         const typeof(((type*)0)->member)* __mptr = (ptr); \
