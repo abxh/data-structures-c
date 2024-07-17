783eaadd65b4bf76bbbd273f732bec3eb47cac6a
Shamim <abir09bs@gmail.com>
2024-07-17 23:59:26 +0200

    add test for align_forward

---
+++ tests/align_forward/align_forward.c
@@ -0,0 +1,41 @@
+/*
+    Pointer offsets (P):
+    P := 0
+    ...
+    P := 128
+
+    Alignments (A):
+    A := 1
+    A := 2
+    A := 4
+    A := 8
+
+    align_forward returns either P, if (P % A) is 0,
+    or P + A - (P % A) otherwise.
+*/
+
+#include "align_forward.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+int main(void) {
+    uintptr_t* P = calloc(129, sizeof(uintptr_t));
+    for (size_t i = 0; i < 129; i++) {
+        P[i] = i;
+    }
+
+    uintptr_t A[] = {1, 2, 4, 8};
+
+    for (size_t i = 0; i < 129; i++) {
+        for (size_t j = 0; j < sizeof(A) / sizeof(*A); j++) {
+            if (P[i] % A[j] == 0) {
+                assert(align_forward(P[i], A[j]) == P[i]);
+            } else {
+                assert(align_forward(P[i], A[j]) == P[i] + A[j] - (P[i] % A[j]));
+            }
+        }
+    }
+
+    free(P);
+}
