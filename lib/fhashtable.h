7b2ae46c85bb4df8ae7866cb835a67286c70e024
Shamim <abir09bs@gmail.com>
2024-07-18 15:44:01 +0200

    add licensing notice

--- lib/fhashtable.h
+++ lib/fhashtable.h
@@ -1,3 +1,23 @@
+/*  fhashtable.h
+ *
+ *  Copyright (C) 2023 abfish
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ */
+
 /**
  * @file fhashtable.h
  * @brief Fixed-size open-adressing hashtable (robin hood hashing).
@@ -19,7 +39,7 @@
  *  @li `KEY_IS_EQUAL(a,b)`
  *  @li `HASH_FUNCTION(key)`
  *
- * Source(s) used for inspiration:
+ * Source(s) used:
  *  @li https://thenumb.at/Hashtables/#robin-hood-linear-probing
  *  @li https://www.sebastiansylvan.com/post/robin-hood-hashing-should-be-your-default-hash-table-implementation/
  */
