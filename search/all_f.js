a8e2180d04e09c8087aad8a71db74b2893acce5b
abxh <abxh@users.noreply.github.com>
2024-07-28 23:12:57 +0000

    deploy: 460ad243856ab3f2fcc105a516bc3a598d7687c0

--- search/all_f.js
+++ search/all_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['round_5fup_5fpow2_0',['round_up_pow2',['../round__up__pow2_8h.html#aeb1f6516cf6ffc3107d86d7ee7e47569',1,'round_up_pow2.h']]],
-  ['round_5fup_5fpow2_2eh_1',['round_up_pow2.h',['../round__up__pow2_8h.html',1,'']]]
+  ['round_5fup_5fpow2_2eh_0',['round_up_pow2.h',['../round__up__pow2_8h.html',1,'']]],
+  ['round_5fup_5fpow2_5f32_1',['round_up_pow2_32',['../round__up__pow2_8h.html#ac941ab6788f53c3a24b85c5ae53ec954',1,'round_up_pow2.h']]]
 ];
