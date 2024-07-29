5d8ead11824d59f16b288a768318cd7933e38e42
Shamim <abir09bs@gmail.com>
2024-07-29 04:43:55 +0200

    fix test

--- tests/round_up_pow2/round_up_pow2.c
+++ tests/round_up_pow2/round_up_pow2.c
@@ -16,14 +16,14 @@
 
 int main(void) {
     {
-        assert(round_up_pow2(1) == 1);
-        assert(round_up_pow2(2) == 2);
-        assert(round_up_pow2(127) == 128);
-        assert(round_up_pow2(128) == 128);
-        assert(round_up_pow2(129) == 256);
-        assert(round_up_pow2(768) == 1024);
-        assert(round_up_pow2(1e+6) == (int)pow(2, round(log2(1e+6))));
-        assert(round_up_pow2(1e+9) == (int)pow(2, round(log2(1e+9))));
-        assert(round_up_pow2(UINT32_MAX / 4) == (int)pow(2, 30));
+        assert(round_up_pow2_32(1) == 1);
+        assert(round_up_pow2_32(2) == 2);
+        assert(round_up_pow2_32(127) == 128);
+        assert(round_up_pow2_32(128) == 128);
+        assert(round_up_pow2_32(129) == 256);
+        assert(round_up_pow2_32(768) == 1024);
+        assert(round_up_pow2_32(1e+6) == (int)pow(2, round(log2(1e+6))));
+        assert(round_up_pow2_32(1e+9) == (int)pow(2, round(log2(1e+9))));
+        assert(round_up_pow2_32(UINT32_MAX / 4) == (int)pow(2, 30));
     }
 }
