72dcc7a43709ac7fd54f839475c08cb77dd7134c
abxh <abxh@users.noreply.github.com>
2024-09-04 19:32:02 +0000

    deploy: d7e96b31130f37f372cdcf84a5d24d20a2680822

--- search/classes_1.js
+++ search/classes_1.js
@@ -5,5 +5,7 @@ var searchData=
   ['fpqueue_5felement_5ftype_2',['fpqueue_element_type',['../structfpqueue__element__type.html',1,'']]],
   ['fpqueue_5ftype_3',['fpqueue_type',['../structfpqueue__type.html',1,'']]],
   ['fqueue_5ftype_4',['fqueue_type',['../structfqueue__type.html',1,'']]],
-  ['fstack_5ftype_5',['fstack_type',['../structfstack__type.html',1,'']]]
+  ['freelist_5fallocation_5fheader_5',['freelist_allocation_header',['../structfreelist__allocation__header.html',1,'']]],
+  ['freelist_5ftype_6',['freelist_type',['../structfreelist__type.html',1,'']]],
+  ['fstack_5ftype_7',['fstack_type',['../structfstack__type.html',1,'']]]
 ];
