4eedccd4489285bf16db9b42bb1e34e49c1cfc8c
abxh <83485102+abxh@users.noreply.github.com>
2026-08-20 16:23:07 +0200

    remove duplicate license notice

--- arena/arena_template.h
+++ arena/arena_template.h
@@ -1,17 +1,6 @@
 // Copyright (c) 2026 abxh
 // SPDX-License-Identifier: MIT
 
-/*  arena_template.h
- *
- *  Copyright (C) 2023 abxh
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *  See the file LICENSE included with this distribution for more
- *  information. */
-
 /**
  * @file arena_template.h
  * @brief Arena allocator
